a9c45b6680dbc163bad54af5676f29f029f0facf
__charitini_chalakateva__ <100290749+CharitiniChalakateva@users.noreply.github.com>
2023-05-06 18:49:35 +0300

    game

--- code/main.cpp
+++ code/main.cpp
@@ -3,36 +3,40 @@
 #include "game.h"
 #include "player.h"
 
-int main(){
-  //main program
-int minNumber = 1;
-int maxNumber = 10000;
-int maxAttempts = 5;
+void play(int minNumber, int maxNumber, int maxAttempts) {
+    // Welcome message
+    std::cout << "Welcome to the CRYPTO GAME! Your objective is to guess the number thar your opponent chose." << std::endl;
+    std::cout << "You will have " << maxAttempts << " attempts to guess the number which is between " << minNumber << " and " << maxNumber << "." << std::endl;
 
-std::cout << "Welcome to the CRYPTO GAME! Your objective is to guess the number thar your opponent chose." << std::endl;
-std::cout << "You will have"<<maxAttempts<< "attempts to guess the number which is between"<< minNumber << "and"<< maxNumber<<"."
-<< std::endl;
+    // Initialization of the game
+    Game cryptGame(minNumber, maxNumber);
+    cryptGame.selectRandomNumber(); // randomly selects a number between the minimum and maximum number set in the game object
 
-// Initialization of the game
-Game cryptGame(minNumber, maxNumber);
-cryptGame.selectRandomNumber(); // randomly selects a number between the minimum and maximum number set in the game object
+    // Play the game
+    Player player; //Creates a new object of the Player class and it's used to store player information during gameplay.
+    bool isGuessCorrect = false;
 
-// Play the game
-Player player; //Creates a new object of the Player class and it's used to store player information during gameplay.
-bool isGuessCorrect = false;
+    for (int i = 1; i <= maxAttempts; i++) {
+        std::cout <<"Attempt " << i << ": ";
+        int guess = player.makeGuess();
+        isGuessCorrect = cryptGame.checkGuess(guess);
+        if (isGuessCorrect) {
+            std::cout << "Congratulations, you guessed the number in " << i << " attempts!" << std::endl;
+            break;
+        }
+    }
 
-for (int i = 1; i <= maxAttempts; i++) {
-   std::cout <<"Attempt"<< i <<":";
-   int guess = player.makeGuess();
-   isGuessCorrect = cryptGame.checkGuess(guess);
-    if (isGuessCorrect) {
-        std::cout << "Congratulations, you guessed the number in" << "attempts!" <<std::endl;
-        break;
+    if (!isGuessCorrect) {
+        std::cout << "Sorry, you have used all your attempts. The number was " << cryptGame.getRandomNumber() << std::endl;
     }
 }
-if (!isGuessCorrect) {
-    std::cout << "Sorry, you have used all your attempts. The number was"<< cryptGame.getRandomNumber() << std::
-    }
-// end of program
-  return 0; //return value 0 for successful completion
+
+int main(){
+    int minNumber = 1;
+    int maxNumber = 10000;
+    int maxAttempts = 5;
+
+    play(minNumber, maxNumber, maxAttempts);
+
+    return 0;
 }
