89ca5894e003d800c39a72200ecb041287e3ca3a
__charitini_chalakateva__ <100290749+CharitiniChalakateva@users.noreply.github.com>
2023-05-06 17:31:59 +0300

    game

--- code/main.cpp
+++ code/main.cpp
@@ -6,7 +6,7 @@
 int main(){
 int minNumber = 1;
 int maxNumber = 10000;
-int maxAttempts = 4;
+int maxAttempts = 5;
 
 std::cout << "Welcome to the Game Crypto! The objective is to guess the number thar your opponent chose." << std::endl;
 std::cout << "You will have"<<maxAttempts<< "attempts to guess the number which is between"<< minNumber << "and"<< maxNumber<<"."
